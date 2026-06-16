7548021c577c9f5819322c80d11c5aa0e00d549f
Shailja Sharma <shailjasharma2307@gmail.com>
2026-06-16 17:33:41 +0530

    added iFrames

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,29 +1,37 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
-        Debug.Log("Current Health: " + currentHealth);
+        // Debug.Log("Current Health: " + currentHealth);
 
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            // iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -37,12 +45,23 @@ public class Health : MonoBehaviour
         }
     }
 
-    // private void Update()
-    // {
-    //     if (Keyboard.current.eKey.wasPressedThisFrame)
-    //     {
-    //         TakeDamage(1);
-    //         Debug.Log("key pressed");
-    //     }
-    // }
+    public void AddHealth(float healthToAdd)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + healthToAdd, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2) );
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2) );
+        }   
+
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+    }
 }
